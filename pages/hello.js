b0ae6ced6b3e7dc493e8f96d267a8614c66d8940
Moose <gabrielmarianoofficial@gmail.com>
2022-09-20 15:58:39 +0800

    added tailwind css

---
+++ pages/hello.js
@@ -0,0 +1,9 @@
+import styles from '../styles/hello.module.css'
+
+const Hello = () => {
+    return (
+        <div className={styles.container}>Hello World</div>
+    )
+}
+
+export default Hello;
